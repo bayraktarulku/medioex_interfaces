e2dbda8611a3676cf73b25d2cdbac2f069546605
nejdetckenobi <nejdetyucesoy@gmail.com>
2017-06-01 13:11:24 +0300

    Path in the model issue fixed.

--- zigbee/models.py
+++ zigbee/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from config import DB
 
 Base = declarative_base()
